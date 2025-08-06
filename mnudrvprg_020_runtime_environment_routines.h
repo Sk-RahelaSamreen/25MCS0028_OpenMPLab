6d2c6ffdeef72a3290a9a97a94dd3bb720d5fb30
Sk-RahelaSamreen <shaikrahelasamreen@gmail.com>
2025-08-06 22:41:47 +0530

    Update mnudrvprg_020_runtime_environment_routines.h

--- mnudrvprg_020_runtime_environment_routines.h
+++ mnudrvprg_020_runtime_environment_routines.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <cstdlib>
-#include "ret_et_hellokaizer.h"
-#include "ret_tid_hellokaizer.h"
+#include "ret_et_helloworld.h"
+#include "ret_tid_helloworld.h"
 
 
 
@@ -10,30 +10,30 @@ int fn_mnudrvprg_020_runtime_environment_routines() {
 	int choice;
 	while (1) {
 		printf("\n\n------------ Menu Driven Program ------------");
-		printf("\n\n1. Hello Kaizer serial program for Runtime Environment Routines \n");
-		printf("2. Hello Kaizer parallel program for Runtime Environment Routines \n");
-		printf("3. Hello Kaizer parallel program for Thread ID\n");
-		printf("4. Hello Kaizer parallel program for Thread ID\n");
+		printf("\n\n1. Hello world serial program for Runtime Environment Routines \n");
+		printf("2. Hello world parallel program for Runtime Environment Routines \n");
+		printf("3. Hello world parallel program for Thread ID\n");
+		printf("4. Hello world parallel program for Thread ID\n");
 		printf("5. Exit \n");
 		printf("Enter Your Choice\n");
 		scanf_s("%d", &choice);
 
 		switch (choice) {
 
-		case 1:printf("\nThis is the output of Hello Kaizer serial program for Runtime Environment \n");
-			fn_ret_et_hellokaizer_srl();
+		case 1:printf("\nThis is the output of Hello world serial program for Runtime Environment \n");
+			fn_ret_et_helloworld_srl();
 			break;
 
-		case 2:printf("\nThis is the output of Hello Kaizer parallel program for Runtime Environment \n");
-			fn_ret_et_hellokaizer_prl();
+		case 2:printf("\nThis is the output of Hello world parallel program for Runtime Environment \n");
+			fn_ret_et_helloworld_prl();
 			break;
 
-		case 3:printf("\nThis is the output of Hello Kaizer parallel program for Thread ID  \n");
-			fn_ret_tid_hellokaizer_srl();
+		case 3:printf("\nThis is the output of Hello world parallel program for Thread ID  \n");
+			fn_ret_tid_helloworld_srl();
 			break;
 
-		case 4:printf("\nThis is the output of Hello Kaizer parallel program for Thread ID \n");
-			fn_ret_tid_hellokaizer_prl();
+		case 4:printf("\nThis is the output of Hello world parallel program for Thread ID \n");
+			fn_ret_tid_helloworld_prl();
 			break;
 
 		case 5: printf("\nYou opted Exit program \n");
